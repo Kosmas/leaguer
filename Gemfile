f8b6870e6fe28bd9ac2dd95520f506a4bbfc3d6b
Kosmas Chatzimichalis <kosmas@mach7x.com>
2013-09-28 13:36:26 +0200

    added coveralls for test coverage

--- Gemfile
+++ Gemfile
@@ -51,3 +51,5 @@ end
 group :test do
   gem "capybara", "2.1.0"
 end
+
+gem "coveralls", require: false
--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,7 @@
 # Load the Rails application.
 require File.expand_path('../application', __FILE__)
+require "coveralls"
+Coveralls.wear!('rails')
 
 # Initialize the Rails application.
 Leaguer::Application.initialize!
