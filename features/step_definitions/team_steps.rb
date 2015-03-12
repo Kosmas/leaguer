a86797df542b06b3e8cbfefc4406dc5304804716
Kosmas Chatzimichalis <kosmas@mach7x.com>
2015-03-12 13:04:22 +0100

    add scenario and steps for creating a team

--- features/step_definitions/team_steps.rb
+++ features/step_definitions/team_steps.rb
@@ -10,3 +10,18 @@ Then(/^I should see the team's name$/) do
   expect(page).to have_content(@team.name)
 end
 
+Given(/^I am on the home page$/) do
+  visit(root_path)
+end
+
+When(/^I add a new team$/) do
+  Team.delete(:all)
+  click_on('New Team')
+  fill_in 'Name', with: 'Flyers'
+  click_on('Create Team')
+end
+
+Then(/^I should have a new team$/) do
+  expect(Team.count).to eq(1)
+end
+
