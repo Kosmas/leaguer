f7907a5b91468c5678f5306a51f35d88afd23ca4
Kosmas Chatzimichalis <kosmas@mach7x.com>
2015-03-14 14:45:59 +0100

    replaced tabs with spaces

--- spec/features/deleting_players_spec.rb
+++ spec/features/deleting_players_spec.rb
@@ -1,19 +1,19 @@
 require 'spec_helper'
 
 feature 'Deleting players' do
-	let! (:team) { FactoryGirl.create(:team) }
-	let! (:player) { FactoryGirl.create(:player, team: team) }
+  let! (:team) { FactoryGirl.create(:team) }
+  let! (:player) { FactoryGirl.create(:player, team: team) }
 
-	before do
-		visit '/'
-		click_link team.name
-		click_link player.surname
-	end
+  before do
+    visit '/'
+    click_link team.name
+    click_link player.surname
+  end
 
-	scenario 'Deleting a player' do
-		click_link 'Delete Player'
+  scenario 'Deleting a player' do
+    click_link 'Delete Player'
 
-		expect(page).to have_content('Player has been deleted.')
-		expect(page.current_url).to eq(team_url(team))
-	end
+    expect(page).to have_content('Player has been deleted.')
+    expect(page.current_url).to eq(team_url(team))
+  end
 end
--- spec/features/editing_players_spec.rb
+++ spec/features/editing_players_spec.rb
@@ -1,33 +1,33 @@
 require 'spec_helper'
 
 feature 'Editing players' do
-	let!(:team) { FactoryGirl.create(:team) }
-	let!(:player) { FactoryGirl.create(:player, team: team) }
+  let!(:team) { FactoryGirl.create(:team) }
+  let!(:player) { FactoryGirl.create(:player, team: team) }
 
-	before do
-		visit '/'
-		click_link team.name
-		click_link player.surname
-		click_link 'Edit Player'
-	end
+  before do
+    visit '/'
+    click_link team.name
+    click_link player.surname
+    click_link 'Edit Player'
+  end
 
-	scenario 'Updating a player' do
-		fill_in 'First name', with: 'Jan'
-		click_button 'Update Player'
+  scenario 'Updating a player' do
+    fill_in 'First name', with: 'Jan'
+    click_button 'Update Player'
 
-		expect(page).to have_content('Player has been updated.')
+    expect(page).to have_content('Player has been updated.')
 
-		within('#player h2') do
-			expect(page).to have_content('Jan')
-		end
+    within('#player h2') do
+      expect(page).to have_content('Jan')
+    end
 
-		expect(page).to_not have_content player.first_name
-	end
+    expect(page).to_not have_content player.first_name
+  end
 
-	scenario 'Updating a player with invalid information' do
-		fill_in 'First name', with: ''
-		click_button 'Update Player'
+  scenario 'Updating a player with invalid information' do
+    fill_in 'First name', with: ''
+    click_button 'Update Player'
 
-		expect(page).to have_content('Player has not been updated.')
-	end
+    expect(page).to have_content('Player has not been updated.')
+  end
 end
