71a292f27c868bc2548ac8676e5864739f1aa23d
Kosmas Chatzimichalis <kosmas@mach7x.com>
2015-03-14 14:32:03 +0100

    replaced tabs with spaces

--- spec/factories/player_factory.rb
+++ spec/factories/player_factory.rb
@@ -1,7 +1,7 @@
 FactoryGirl.define do
-	factory :player do
-		first_name 'Pete'
-		surname 'Wozer'
-		dob '1988-12-14'
-	end
+  factory :player do
+    first_name 'Pete'
+    surname 'Wozer'
+    dob '1988-12-14'
+  end
 end
--- spec/factories/user_factory.rb
+++ spec/factories/user_factory.rb
@@ -1,8 +1,8 @@
 FactoryGirl.define do
-	factory :user do
-		name 'username'
-		email 'test@test.com'
-		password 'password'
-		password_confirmation 'password'
-	end
+  factory :user do
+    name 'username'
+    email 'test@test.com'
+    password 'password'
+    password_confirmation 'password'
+  end
 end
