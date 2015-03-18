f291a97e45efcd3fb86963050e101b644db8f80e
Kosmas Chatzimichalis <kosmas@mach7x.com>
2015-03-18 14:55:18 +0100

    add feature and step definitions for viewing a season

--- features/step_definitions/season_steps.rb
+++ features/step_definitions/season_steps.rb
@@ -9,3 +9,15 @@ end
 Then(/^I should have a new season$/) do
   expect(Season.count).to eq(1)
 end
+
+Given(/^there is a season "(.*?)"$/) do |season_descr|
+  @season = FactoryGirl.create(:season, description: season_descr)
+end
+
+When(/^I go to the seasons page$/) do
+  visit '/seasons'
+end
+
+Then(/^I should see the season's description$/) do
+  expect(page).to have_content(@season.description)
+end
