c14ee6ccdee15f2acacbd2419f4935cf19a2ca77
Kosmas Chatzimichalis <kosmas@mach7x.com>
2015-10-24 17:02:59 +0200

    add class documentation

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -1,3 +1,4 @@
+# Class that deals with teams
 class TeamsController < ApplicationController
   before_action :set_team, only: [:show, :edit, :update, :destroy]
   before_action :set_seasons, only: [:create, :update]
