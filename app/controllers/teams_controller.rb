261e6bfa8419b4848239ad45941e8f9c77c15db6
Kosmas Chatzimichalis <kosmas@mach7x.com>
2015-10-24 16:58:27 +0200

    add comments to teams actions and methods

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -2,14 +2,17 @@ class TeamsController < ApplicationController
   before_action :set_team, only: [:show, :edit, :update, :destroy]
   before_action :set_seasons, only: [:create, :update]
 
+  # GET /teams
   def index
     @teams = Team.all
   end
 
+  # GET /teams/new
   def new
     @team = Team.new
   end
 
+  # POST /teams
   def create
     @team = Team.new(team_params)
     @team.seasons |= @seasons
@@ -24,12 +27,15 @@ class TeamsController < ApplicationController
     end
   end
 
+  # GET /teams/1
   def show
   end
 
+  # GET /teams/1/edit
   def edit
   end
 
+  # PATCH/PUT /teams/1
   def update
     @team.seasons |= @seasons
 
@@ -42,6 +48,7 @@ class TeamsController < ApplicationController
     end
   end
 
+  # DELETE /teams/1
   def destroy
     @team.destroy
 
@@ -52,10 +59,12 @@ class TeamsController < ApplicationController
 
   private
 
+  # Never trust parameters from the scary internet, only allow the white list through.
   def team_params
     params.require(:team).permit(:name, :address1)
   end
 
+  # Use callbacks to share common setup or constraints between actions.
   def set_team
     @team = Team.find(params[:id])
     rescue ActiveRecord::RecordNotFound
@@ -63,6 +72,7 @@ class TeamsController < ApplicationController
     redirect_to teams_path
   end
 
+  # Use callbacks to share common setup or constraints between actions.
   def set_seasons
     @seasons = Season.where(id: params[:seasons])
   end
